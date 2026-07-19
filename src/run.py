2553b854ae30bb40ce591b7e91674467f15204ac
csopetap <csoka.peter.05@gmail.com>
2026-07-19 18:22:11 +0200

    Peak_Selection_Check

--- src/run.py
+++ src/run.py
@@ -1,7 +1,7 @@
 from plotting import*
 from spectrum_export import *
 
-Input = ['Cs_Papir_R', 'BG_04_21_R']
+Input = ['Cs_Viz_R', 'BG_04_21_R']
 Cs_P, BG = get_Spectra_from_mca(Input)
 Cs_noBG = subtract_spectra(Cs_P, BG)
 plot_Spectrum([Cs_P, Cs_noBG, BG], 'log', energy_min= 0, energy_max=1500, show_errors=True)
